3b87ac046b529079bab9799a2b8dc716b8be89d8
Mst.Joygun Nesa <97111156+joygunnesa10@users.noreply.github.com>
2022-04-21 06:57:10 -0700

    Delete negative_image.py

--- python_code/negative_image.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread('E:/Image_processing/python_code/images/rose.jpg')
-print (img.dtype)
-img_neg = 255 - img
-cv2.imshow('negative',img_neg)
-cv2.waitKey(0)
